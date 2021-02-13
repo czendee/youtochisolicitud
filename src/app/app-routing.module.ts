d26bfb5f68864cc696db8cacf72a3c8c360f76dd
Don Carlos Zendejas <czende@hotmail.com>
2021-02-13 08:56:55 -0600

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmpresaCreateComponent } from './empresa-create/empresa-create.component';
 import { EmpresaEditComponent } from './empresa-edit/empresa-edit.component';
-import { EmpresaListComponent } from './empresa-list/empresa-list.component';
+import { EmpresasListComponent } from './empresas-list/empresas-list.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'create-empresa' },
   { path: 'create-empresa', component: EmpresaCreateComponent },
-  { path: 'empresas-list', component: EmpresaListComponent },
+  { path: 'empresas-list', component: EmpresasListComponent },
   { path: 'empresa-edit/:id', component: EmpresaEditComponent }  
 ];
 
