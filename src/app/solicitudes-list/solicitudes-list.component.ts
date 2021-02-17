e6d62fa528beb4569e094442d12db46c83f26703
Don Carlos Zendejas <czende@hotmail.com>
2021-02-17 15:41:45 -0600

    Update and rename src/app/servicios-list/servicios-list.component.ts to src/app/solicitudes-list/solicitudes-list.component.ts

---
+++ src/app/solicitudes-list/solicitudes-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { RestApiService } from "../shared/rest-api.service";
+
+@Component({
+  selector: 'app-solicitudes-list',
+  templateUrl: './solicitudes-list.component.html',
+  styleUrls: ['./solicitudes-list.component.css']
+})
+export class SolicitudesListComponent implements OnInit {
+
+  Solicitud: any = [];
+
+  constructor(
+    public restApi: RestApiService
+  ) { }
+
+  ngOnInit() {
+    this.loadSolicitudes()
+  }
+
+  // Get solicitudes list
+  loadSolicitudes() {
+    return this.restApi.getSolicitudes().subscribe((data: {}) => {
+      this.Solicitud = data;
+    })
+  }
+
+  // Delete solicitud
+  deleteSolicitud(id) {
+    if (window.confirm('Are you sure, you want to delete?')){
+      this.restApi.deleteSolicitudo(id).subscribe(data => {
+        this.loadSolicitudes()
+      })
+    }
+  }  
+
+}
